06acb0f90f3fa59ca2e99ac6ab8ff5bd5f3f8f63
vipeilko <ville.kouhia@gmail.com>
2020-03-06 18:57:33 +0200

    introduction of client
    Just form to enter the code. Link to login page next.

---
+++ API/image.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * To generate background images and logos etc.... 
+ * + 06.03.2020 Introduction of generating random backgroundimage
+ * 
+ */
+
+if ( $_GET['image'] == "background.png" ) {
+    header('Content-type: image/png');
+    $png_image = imagecreate(1920, 1080);
+    $white = imagecolorallocate($png_image, 255, 255, 255);
+    $black = imagecolorallocate($png_image, 0,0,0);
+
+    imagefilltoborder($png_image, 0, 0, $white, $white);
+    
+    $k = rand(50,100);
+    
+    for ( $i = 0; $i <= $k; $i++ ) {
+        $color = imagecolorallocate($png_image, rand(50,200), rand(50,200), rand(50,200));
+        $squareSize = rand(20,100);
+        $x = rand(20,1900-$squareSize);
+        $y = rand(20,1060-$squareSize);
+        $x2 = $x+$squareSize;
+        $y2 = $y+$squareSize;
+        //border is 2px larger than square it self
+        imagefilledrectangle ($png_image, ($x-2), ($y-2), ($x2+2), ($y2+2), $black);
+        // square top of border
+        imagefilledrectangle ($png_image, $x, $y, $x2, $y2, $color);  
+        
+    }
+    
+    imagepng($png_image);
+    imagedestroy($png_image);
+}
+
+?>
---
+++ client/index.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PhotoShare</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Arvo&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="container">
+	<div class="box a"></div>
+	<div class="box b"></div>
+	<div class="box c"></div>
+	<div class="box d"></div>
+	<div class="box e">
+		<div class="box header">ENTER CODE</div>
+		<div class="box content">
+			<form>
+        		<input maxlength="8" size="12" value="12345678" type="text" id="code" name="code">
+        		<input type="submit" id="send">
+    		</form>
+		</div>
+	</div>
+  	<div class="box f"></div>
+  	<div class="box g"></div>
+  	<div class="box h"></div>
+  	<div class="box i"></div>  	
+</div>
+
+
+</body>
+
+</html>
